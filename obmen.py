d79e214bd3c036ec99f09d03de3496b8368c3823
ToHka12-bit <mishin327@gmail.com>
2025-10-02 13:16:14 +0300

    Изменён формат вывода курса. Теперь выводится только 2 символа после точки.

--- obmen.py
+++ obmen.py
@@ -13,7 +13,7 @@ def exchange():
             data = response.json()
             if code in data["rates"]:
                 exchange_rate = data["rates"][code]
-                mb.showinfo("Курс обмена", f'Курс: {exchange_rate} {code} за 1 рубль')
+                mb.showinfo("Курс обмена", f'Курс: {exchange_rate:.2f} {code} за 1 долар')
             else:
                 mb.showerror("Ошибка!", f"Валюта {code} не найдена!")
         except Exception as e:
